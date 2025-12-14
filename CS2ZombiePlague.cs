e34aeabd98d881a69c700a0c6bdfa4c7d570edf1
luciferkaz <98942560+luciferkaz@users.noreply.github.com>
2025-12-14 23:24:27 +0700

    Initial commit

---
+++ CS2ZombiePlague.cs
@@ -0,0 +1,15 @@
+﻿using CounterStrikeSharp.API.Core;
+
+namespace CS2ZombiePlague
+{
+    public class CS2ZombiePlague : BasePlugin
+    {
+        public override string ModuleName => "CS2ZombiePlague";
+
+        public override string ModuleVersion => "0.0.1";
+
+        public override void Load(bool hotReload)
+        {
+        }
+    }
+}
